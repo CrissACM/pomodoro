d03d1016e8b4960393cf5b22d9e27d8b5901cbb9
Cristobal Colmenares <alex.ccm7Z@gmail.com>
2023-08-14 16:48:08 -0400

    initial project

--- App.js
+++ App.js
@@ -1,11 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Audio } from 'expo-av';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Header from './src/components/Header';
+import Timer from './src/components/Timer';
+
+const colors = ['#F7DC6F', '#A2D9CE', '#D7BDE2'];
 
 export default function App() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState('POMO' | 'SHORT' | 'BREAK');
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime(time - 1);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    if (time === 0) {
+      setIsActive(false);
+      setIsWorking((prev) => !prev);
+      setTime(isWorking ? 300 : 1500);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, time]);
+
+  function handleStartStop() {
+    playSound();
+    setIsActive(!isActive);
+  }
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require('./assets/click.mp3')
+    );
+    await sound.playAsync();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: colors[currentTime] }]}>
+      <Text style={styles.text}>Pomodoro</Text>
+      <Header
+        currentTime={currentTime}
+        setCurrentTime={setCurrentTime}
+        setTime={setTime}
+      />
+      <Timer time={time} />
+      <TouchableOpacity onPress={handleStartStop} style={styles.button}>
+        <Text style={{ color: 'white', fontWeight: 'bold' }}>
+          {isActive ? 'STOP' : 'START'}
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,7 +66,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    paddingTop: 30,
+    paddingHorizontal: 15,
+  },
+  text: {
+    fontSize: 32,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  button: {
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#333333',
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
   },
 });
